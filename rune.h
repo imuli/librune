cd37a27d4cedb05c060768c2d029ba3ea88f7ffb
Imuli <i@imu.li>
2015-04-22 13:16:40 -0400

    is_rune_classifier: lookup character and bidi classes

--- rune.h
+++ rune.h
@@ -13,6 +13,40 @@ enum {
 	BadRune = 0xffff,
 };
 
+enum classes {
+	/* character classes */
+	UnknC = 0x000, Cc, Cf, Co, Cs, Ll,
+	Lm, Lo, Lt, Lu, Mc, Me,
+	Mn, Nd, Nl, No, Pc, Pd,
+	Pe, Pf, Pi, Po, Ps, Sc,
+	Sk, Sm, So, Zl, Zp, Zs,
+	CharClassMask = 0x1f,
+	/* bidi classes */
+	AL = 0x100,
+	AN = 0x200,
+	B = 0x300,
+	BN = 0x400,
+	CS = 0x500,
+	EN = 0x600,
+	ES = 0x700,
+	ET = 0x800,
+	FSI = 0x900,
+	L = 0xa00,
+	LRE = 0xb00,
+	LRI = 0xc00,
+	LRO = 0xd00,
+	NSM = 0xe00,
+	ON = 0xf00,
+	PDF = 0x1000,
+	PDI = 0x1100,
+	R = 0x1200,
+	RLE = 0x1300,
+	RLI = 0x1400,
+	RLO = 0x1500,
+	S = 0x1600,
+	WS = 0x1700,
+};
+
 /* sizes and lengths */
 int rune_bytes(Rune r);
 int rune_n_bytes(Rune* r, int len);
@@ -31,6 +65,7 @@ int rune_width(Rune r);
 
 /* internal */
 struct rune_range { Rune first, last; };
+int is_rune_classifier(Rune r);
 int is_rune_in(struct rune_range *table, int start, int end, Rune r);
 int log2u32(uint32_t r);
 #define nelem(a) sizeof(a)/sizeof(*a)
